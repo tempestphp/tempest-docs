b976a755a4e845ea841451ecc8518c281df1966e
Brent Roose <brent.roose@gmail.com>
2024-03-13 10:18:15 +0100

    Add deploy command

---
+++ app/Commands/DeployCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Commands;
+
+use Tempest\Console\Console;
+use Tempest\Console\ConsoleCommand;
+
+final readonly class DeployCommand
+{
+    public function __construct(private Console $console) {}
+
+    #[ConsoleCommand('deploy')]
+    public function __invoke(): void
+    {
+        $this->console->info("Starting deploy");
+
+        passthru("ssh forge@stitcher.io 'cd tempest.stitcher.io && sh deploy.sh'");
+
+        $this->console->info("Deploy success");
+    }
+}
