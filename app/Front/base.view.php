62e1398f7ea09390c87ff1a365f273c961ff8f32
Ostap Brehin <osbre@protonmail.com>
2024-08-13 21:29:19 +0100

    Use custom font for code

--- app/Front/base.view.php
+++ app/Front/base.view.php
@@ -8,6 +8,8 @@
 
         <x-slot name="styles"/>
         <link href="/main.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=source-code-pro:500" rel="stylesheet" />
 
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
