46600ac132f9602f0e7e6bc86928f51cb9a7c405
brendt <brent.roose@gmail.com>
2024-09-21 20:38:23 +0200

    wip

--- app/Front/Docs/DocsController.php
+++ app/Front/Docs/DocsController.php
@@ -6,13 +6,27 @@ namespace App\Front\Docs;
 
 use App\Chapters\ChapterRepository;
 use Tempest\Http\Get;
+use Tempest\Http\Responses\Redirect;
 use Tempest\Http\StaticPage;
 use Tempest\View\View;
+use function Tempest\uri;
 
 final readonly class DocsController
 {
+    #[Get('/framework/01-getting-started')]
+    public function frameworkIndex(): Redirect
+    {
+        return new Redirect(uri([self::class, '__invoke'], category: 'framework', slug: '01-getting-started'));
+    }
+
+    #[Get('/console/01-getting-started')]
+    public function consoleIndex(): Redirect
+    {
+        return new Redirect(uri([self::class, '__invoke'], category: 'console', slug: '01-getting-started'));
+    }
+    
     #[StaticPage(DocsDataProvider::class)]
-    #[Get('/{category}/{slug}')]
+    #[Get('/docs/{category}/{slug}')]
     public function __invoke(string $category, string $slug, ChapterRepository $chapterRepository): View
     {
         return new DocsView(
