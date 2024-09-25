8878e8f9973e8fdc3e21201be0f77414b3ae9802
Christopher Pitt <cgpitt@gmail.com>
2024-09-25 09:55:55 +0200

    Update codeblock-home.view.php

--- app/Front/Components/codeblock-home.view.php
+++ app/Front/Components/codeblock-home.view.php
@@ -1,5 +1,5 @@
 <x-component name="x-codeblock-home">
-  <div class="overflow-x-scroll max-w-full grid gap-8 col-span-5 md:col-span-3 bg-white px-6 py-8 rounded-[9px] shadow-[0_0_0_1px_rgba(0,0,0,0.1)] text-[14px]">
+  <div class="overflow-x-auto max-w-full grid gap-8 col-span-5 md:col-span-3 bg-white px-6 py-8 rounded-[9px] shadow-[0_0_0_1px_rgba(0,0,0,0.1)] text-[14px]">
     <x-slot />
   </div>
 </x-component>
