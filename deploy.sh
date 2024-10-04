08bab85945d015219bf29c3b7bf54215dc65d453
Brent Roose <brent.roose@gmail.com>
2024-10-04 09:39:53 +0200

    wip

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ php8.3 /usr/local/bin/composer install --no-dev
 npm install
 npm run build
 php8.3 tempest discovery:clear
+php8.3 tempest cache:clear --all
 php8.3 tempest static:clean --force
 php8.3 tempest static:generate
