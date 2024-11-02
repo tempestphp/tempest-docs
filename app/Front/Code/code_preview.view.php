4c8c36ef0c7488e0dd6aaa5e4c738abdce250451
brendt <brent.roose@gmail.com>
2024-11-02 08:03:14 +0100

    wip

--- app/Front/Code/code_preview.view.php
+++ app/Front/Code/code_preview.view.php
@@ -2,7 +2,7 @@
 
     <div class="hero-bg h-full flex items-center justify-center">
         <x-codeblock-home class="max-h-[80%]">
-            <pre class="max-w-[66%] overflow-y-scroll"><?= $code ?></pre>
+            <pre><?= $code ?></pre>
         </x-codeblock-home>
     </div>
 
