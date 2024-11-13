463cb66497781d0d8e5e6a16749b33ebb04f4ce0
Brent Roose <brent.roose@gmail.com>
2024-11-13 13:23:43 +0100

    doc updates

--- app/Front/Code/code.view.php
+++ app/Front/Code/code.view.php
@@ -20,8 +20,9 @@ use function Tempest\uri;
     <div class="hero-bg h-full flex items-center justify-center">
         <form action="<?= uri([CodeController::class, 'submit']) ?>" method="POST" class="w-full">
             <div class="grid gap-4 max-full max-w-[66%] mx-auto">
+                <input type="hidden" name="lang" :value="$language">
                 <label for="code" class="text-xl text-white">Paste your code</label>
-                <textarea name="code" id="code" class="border-2 border-[--primary] w-full p-4 rounded bg-[var(--code-background)] text-[var(--foreground)]" rows="20" autofocus></textarea>
+                <textarea name="code" id="code" class="border-2 border-[--primary] w-full p-4 rounded bg-[var(--code-background)] text-[var(--foreground)]" rows="20" autofocus>{{ $code }}</textarea>
                 <div class="flex justify-end">
                     <input type="submit" name="Generate" class="bg-[var(--link-color)] text-white font-bold p-4 py-3 hover:bg-[var(--link-hover-color)] rounded cursor-pointer"/>
                 </div>
