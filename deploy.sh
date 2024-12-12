19cd5e7d2c0aec5f19e3264b317d51db46d47910
Brent Roose <brent.roose@gmail.com>
2024-12-12 20:49:29 +0100

    Add analytics WIP

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ npm install
 npm run build
 php8.4 tempest cache:clear --all
 php8.4 tempest discovery:generate
+php8.4 tempest migrate:up --force
 php8.4 tempest static:clean --force
 php8.4 tempest static:generate
