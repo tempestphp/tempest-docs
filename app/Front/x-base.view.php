932d3fff8ee9cdb85d89ac9f4558546a925ee9c2
brendt <brent.roose@gmail.com>
2025-03-01 20:45:27 +0100

    update

--- app/Front/x-base.view.php
+++ app/Front/x-base.view.php
@@ -24,20 +24,21 @@ use App\Front\Meta\MetaType;
         <meta name="image" content="<?= $metaImageUri ?>"/>
         <meta name="twitter:card" content="summary_large_image"/>
 
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="preconnect" href="https://fonts.bunny.net"/>
         <link href="https://fonts.bunny.net/css?family=source-code-pro:500|archivo:700,900" rel="stylesheet"/>
         <link href="https://cdn.jsdelivr.net/npm/@docsearch/css@3" rel="stylesheet"/>
-        <link href="/main.css" rel="stylesheet">
-
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-        <link rel="manifest" href="/favicon/site.webmanifest">
+        <link href="/main.css" rel="stylesheet"/>
 
         <x-slot name="styles"/>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/favicon/site.webmanifest"/>
+
         <x-slot name="head" />
     </head>
 
