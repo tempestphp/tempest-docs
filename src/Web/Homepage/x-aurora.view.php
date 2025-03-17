c416acd5e1ad17e169136a376b4d985db3f3cab5
Enzo Innocenzi <enzo@innocenzi.dev>
2025-03-17 18:34:24 +0100

    chore: temporarily disable motion-reduce-based classes

--- src/Web/Homepage/x-aurora.view.php
+++ src/Web/Homepage/x-aurora.view.php
@@ -3,20 +3,24 @@
     <div class=" 
           [--color-white-gradient:repeating-linear-gradient(100deg,white_0%,white_7%,transparent_10%,transparent_12%,white_16%)]
           [--color-aurora-gradient:repeating-linear-gradient(100deg,var(--color-aurora-1)_10%,var(--color-aurora-2)_15%,var(--color-aurora-3)_20%,var(--color-aurora-4)_25%,var(--color-aurora-5)_30%)]
-          motion-reduce:[background-image:var(--color-aurora-gradient)]
-          motion-safe:[background-image:var(--color-white-gradient),var(--color-aurora-gradient)]
+          [background-image:var(--color-aurora-gradient)]
+          ___motion-reduce:[background-image:var(--color-aurora-gradient)]
+          ___motion-safe:[background-image:var(--color-white-gradient),var(--color-aurora-gradient)]
           [background-size:300%,_200%]
           motion-safe:[background-position:50%_50%,50%_50%]
+          [background-position:50%_50%,50%_50%]
           filter
           blur-[10px]
           invert
           after:content-['']
           after:absolute
           after:inset-0
-          motion-reduce:after:[background-image:var(--color-aurora-gradient)]
-          motion-safe:after:[background-image:var(--color-white-gradient),var(--color-aurora-gradient)]
+          ___motion-reduce:after:[background-image:var(--color-aurora-gradient)]
+          ___motion-safe:after:[background-image:var(--color-white-gradient),var(--color-aurora-gradient)]
+          after:[background-image:var(--color-white-gradient),var(--color-aurora-gradient)]
           after:[background-size:200%,_100%]
-          motion-safe:after:animate-aurora
+          ___motion-safe:after:animate-aurora
+          after:animate-aurora
           after:[background-attachment:fixed]
           after:mix-blend-difference
           pointer-events-none
