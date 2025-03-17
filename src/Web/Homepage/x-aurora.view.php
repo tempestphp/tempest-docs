3dfd6576ab7bf8b0237f72b034d4b4d013fafd43
Enzo Innocenzi <enzo@innocenzi.dev>
2025-03-17 18:20:19 +0100

    refactor: use `motion-safe` instead of `not-motion-reduce`

--- src/Web/Homepage/x-aurora.view.php
+++ src/Web/Homepage/x-aurora.view.php
@@ -4,9 +4,9 @@
           [--color-white-gradient:repeating-linear-gradient(100deg,white_0%,white_7%,transparent_10%,transparent_12%,white_16%)]
           [--color-aurora-gradient:repeating-linear-gradient(100deg,var(--color-aurora-1)_10%,var(--color-aurora-2)_15%,var(--color-aurora-3)_20%,var(--color-aurora-4)_25%,var(--color-aurora-5)_30%)]
           motion-reduce:[background-image:var(--color-aurora-gradient)]
-          not-motion-reduce:[background-image:var(--color-white-gradient),var(--color-aurora-gradient)]
+          motion-safe:[background-image:var(--color-white-gradient),var(--color-aurora-gradient)]
           [background-size:300%,_200%]
-          not-motion-reduce:[background-position:50%_50%,50%_50%]
+          motion-safe:[background-position:50%_50%,50%_50%]
           filter
           blur-[10px]
           invert
@@ -14,9 +14,9 @@
           after:absolute
           after:inset-0
           motion-reduce:after:[background-image:var(--color-aurora-gradient)]
-          not-motion-reduce:after:[background-image:var(--color-white-gradient),var(--color-aurora-gradient)]
+          motion-safe:after:[background-image:var(--color-white-gradient),var(--color-aurora-gradient)]
           after:[background-size:200%,_100%]
-          not-motion-reduce:after:animate-aurora
+          motion-safe:after:animate-aurora
           after:[background-attachment:fixed]
           after:mix-blend-difference
           pointer-events-none
