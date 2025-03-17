4948a92af390ce764d0d5a2d8e2bafa02f5f01e6
brendt <brent.roose@gmail.com>
2025-03-17 11:13:36 +0100

    log stargazer error

--- src/GitHub/GetStargazersCount.php
+++ src/GitHub/GetStargazersCount.php
@@ -12,25 +12,19 @@ final class GetStargazersCount
     public function __construct(
         private HttpClient $httpClient,
         private Cache $cache,
-    ) {
-    }
+    ) {}
 
     public function __invoke(): ?string
     {
         return $this->cache->resolve(
             key: 'tempest-stargazers',
             cache: function () {
-                try {
-                    $body = $this->httpClient->get('https://api.github.com/repos/tempestphp/tempest-framework')->body;
-                    $stargazers = json_decode($body)->stargazers_count ?? null;
+                $body = $this->httpClient->get('https://api.github.com/repos/tempestphp/tempest-framework')->body;
+                $stargazers = json_decode($body)->stargazers_count ?? null;
 
-                    return $stargazers > 999
-                        ? (round($stargazers / 1000, 1) . 'K')
-                        : $stargazers;
-                } catch (Throwable $e) {
-                    ll($e);
-                    return null;
-                }
+                return $stargazers > 999
+                    ? (round($stargazers / 1000, 1) . 'K')
+                    : $stargazers;
             },
             expiresAt: new DateTimeImmutable('+12 hours'),
         );
