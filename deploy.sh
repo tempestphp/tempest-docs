5bc240e04cfc015e1f09a4bebb65f0a6e90b9554
brendt <brent.roose@gmail.com>
2025-03-18 13:17:25 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 php8.4 /usr/local/bin/composer install --no-dev
 php8.4 tempest discovery:generate
 php8.4 tempest command-palette:index
-bun install
-bun build
+/home/forge/.bun/bin/bun install
+/home/forge/.bun/bin/bun build
 php8.4 tempest cache:clear --all
 php8.4 tempest discovery:generate
 php8.4 tempest migrate:up --force
