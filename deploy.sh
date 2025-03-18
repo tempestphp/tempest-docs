a15c1c1f262a5bbeff6eb90cbaa1bc37f94e0c09
brendt <brent.roose@gmail.com>
2025-03-18 13:23:28 +0100

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 php8.4 /usr/local/bin/composer install --no-dev
 php8.4 tempest discovery:generate
 php8.4 tempest command-palette:index
+/home/forge/.bun/bin/bun --version
 /home/forge/.bun/bin/bun install
 /home/forge/.bun/bin/bun run build
 php8.4 tempest cache:clear --all
