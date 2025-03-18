f7c7d9ea17ee54b9520a0b026980587febd2942f
brendt <brent.roose@gmail.com>
2025-03-18 13:25:30 +0100

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
+# Composer
 php8.4 /usr/local/bin/composer install --no-dev
-php8.4 tempest command-palette:index
-node --version
+
+# Bun
+nvm use node
 /home/forge/.bun/bin/bun --version
 /home/forge/.bun/bin/bun install
 /home/forge/.bun/bin/bun run build
+
+# Tempest
+php8.4 tempest command-palette:index
 php8.4 tempest cache:clear --all
 php8.4 tempest discovery:generate
 php8.4 tempest migrate:up --force
 php8.4 tempest static:clean --force
 php8.4 tempest static:generate
+
+# Supervisor
 sudo supervisorctl restart all
