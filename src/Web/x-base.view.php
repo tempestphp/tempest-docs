b88518a31e185e9b41b66169ee214a650324acee
brendt <brent.roose@gmail.com>
2025-03-18 06:50:04 +0100

    wip

--- src/Web/x-base.view.php
+++ src/Web/x-base.view.php
@@ -51,7 +51,7 @@
 
     <x-slot name="head"/>
 </head>
-<body :class="trim(($bodyClass ?? '') . ' relative antialiased flex flex-col grow')">
+<body :class="trim($bodyClass ?? '')" class="relative antialiased flex flex-col grow">
 <div class="absolute pointer-events-none inset-0 bg-repeat" style="background-image: url(/noise.svg)">
     <div id="command-palette"></div>
 </div>
