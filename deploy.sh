f5c9f6e699980e64f2d79023685c9152cb7f2f65
Enzo Innocenzi <enzo@innocenzi.dev>
2025-03-30 23:21:11 +0200

    fix: generate command palette before building

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ php8.4 /usr/local/bin/composer install --no-dev
 # Bun
 /home/forge/.bun/bin/bun --version
 /home/forge/.bun/bin/bun install
+php8.4 tempest command-palette:index
 /home/forge/.bun/bin/bun run build
 
 # Tempest
-php8.4 tempest command-palette:index
 php8.4 tempest cache:clear --all
 php8.4 tempest discovery:generate
 php8.4 tempest migrate:up --force
