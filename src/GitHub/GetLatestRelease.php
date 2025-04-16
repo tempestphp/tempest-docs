3b6e0a895b46fe5c26ed80b3d21cb72871f7ed22
Aidan Casey <aidan@caseyhouse.net>
2025-04-16 12:19:28 -0400

    Fixes stars.

--- src/GitHub/GetLatestRelease.php
+++ src/GitHub/GetLatestRelease.php
@@ -6,6 +6,8 @@ use Tempest\Core\Kernel;
 use Tempest\HttpClient\HttpClient;
 use Throwable;
 
+use function Tempest\env;
+
 final class GetLatestRelease
 {
     public function __construct(
@@ -15,12 +17,23 @@ final class GetLatestRelease
 
     public function __invoke(): ?string
     {
+        // Added by Aidan Casey to combat the GitHub rate limits.
+        // We will inject the GH_TOKEN using our workflow.
+        $headers = [];
+
+        if ($githubToken = env('GH_TOKEN')) {
+            $headers['Authorization'] = 'Bearer ' . $githubToken;
+        }
+
         // Default release to the currently running version of Tempest.
         $defaultRelease = sprintf('v%s', Kernel::VERSION);
 
         try {
             $body = $this->httpClient
-                ->get('https://api.github.com/repos/tempestphp/tempest-framework/releases/latest')
+                ->get(
+                    uri: 'https://api.github.com/repos/tempestphp/tempest-framework/releases/latest',
+                    headers: $headers
+                )
                 ->body;
 
             return json_decode($body)->tag_name ?? $defaultRelease;
--- src/GitHub/GetStargazersCount.php
+++ src/GitHub/GetStargazersCount.php
@@ -4,6 +4,7 @@ namespace App\GitHub;
 
 use Tempest\HttpClient\HttpClient;
 use Throwable;
+use function Tempest\env;
 
 final class GetStargazersCount
 {
@@ -14,8 +15,19 @@ final class GetStargazersCount
 
     public function __invoke(): ?string
     {
+        // Added by Aidan Casey to combat the GitHub rate limits.
+        // We will inject the GH_TOKEN using our workflow.
+        $headers = [];
+
+        if ($githubToken = env('GH_TOKEN')) {
+            $headers['Authorization'] = 'Bearer ' . $githubToken;
+        }
+
         try {
-            $body = $this->httpClient->get('https://api.github.com/repos/tempestphp/tempest-framework')->body;
+            $body = $this->httpClient->get(
+                uri: 'https://api.github.com/repos/tempestphp/tempest-framework',
+                headers: $headers
+            )->body;
             $stargazers = json_decode($body)->stargazers_count ?? null;
 
             return $stargazers > 999
