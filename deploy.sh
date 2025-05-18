06e87bbec6a9ef72c1c560f91772eea3f06e6f89
Enzo Innocenzi <enzo@innocenzi.dev>
2025-05-18 23:21:06 +0200

    build: update deploy script instruction order

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,20 @@
 . /home/forge/.bashrc
 . ~/.nvm/nvm.sh
 
-# Composer
+# Dependencies
 php8.4 /usr/local/bin/composer install --no-dev
-
-# Bun
 /home/forge/.bun/bin/bun --version
 /home/forge/.bun/bin/bun install
-php8.4 tempest command-palette:index
-/home/forge/.bun/bin/bun run build
 
 # Tempest
 php8.4 tempest cache:clear --all
 php8.4 tempest discovery:generate
 php8.4 tempest migrate:up --force
 php8.4 tempest static:clean --force
+
+# Build front-end
+php8.4 tempest command-palette:index
+/home/forge/.bun/bin/bun run build
 php8.4 tempest static:generate --allow-dead-links --verbose=true
 
 # Supervisor
