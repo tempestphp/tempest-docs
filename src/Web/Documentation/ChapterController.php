539ee316881acd19fe424292d564025432f7391c
brendt <brent.roose@gmail.com>
2025-07-29 08:57:03 +0200

    wip

--- src/Web/Documentation/ChapterController.php
+++ src/Web/Documentation/ChapterController.php
@@ -25,8 +25,6 @@ final readonly class ChapterController
         return new Redirect(sprintf('/%s/%s', Version::default()->value, $path));
     }
 
-    #[Get('/docs')]
-    #[Get('/docs/')]
     #[Get('/documentation')]
     #[Get('/main/framework/getting-started')]
     public function index(): Redirect
