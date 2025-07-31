262f01412c228a4c46e761b543a4409da9ee7388
Enzo Innocenzi <enzo@innocenzi.dev>
2025-07-31 21:22:57 +0200

    feat: autodiscover rss

--- src/Web/x-base.view.php
+++ src/Web/x-base.view.php
@@ -3,7 +3,7 @@ use App\Web\Meta\MetaType;
 
 ?>
 
-<html lang="en" class="h-dvh flex flex-col scroll-smooth">
+<html lang="en" class="flex flex-col h-dvh scroll-smooth">
 <head>
     <!-- Meta title -->
     <?php
@@ -17,6 +17,7 @@ use App\Web\Meta\MetaType;
     <title>{{ $title }}</title>
 
     <link :if="$meta['canonical'] ?? null" rel="canonical" :href="$meta['canonical']" />
+    <link rel="alternate" type="application/rss+xml" title="Tempest RSS" href="https://tempestphp.com/rss" />
 
     <meta name="title" :content="$title">
     <meta name="twitter:title" :content="$title">
@@ -79,7 +80,7 @@ use App\Web\Meta\MetaType;
     <x-slot name="head"/>
 </head>
 <body :class="trim($bodyClass ?? '')" class="relative antialiased flex flex-col grow selection:bg-(--ui-primary)/20 selection:text-(--ui-primary) font-sans text-(--ui-text) bg-(--ui-bg) scheme-light dark:scheme-dark !overflow-visible !pr-0">
-<div class="absolute pointer-events-none inset-0 bg-repeat" style="background-image: url(/noise.svg)">
+<div class="absolute inset-0 bg-repeat pointer-events-none" style="background-image: url(/noise.svg)">
     <div id="command-palette"></div>
 </div>
 <x-header :stargazers="$this->stargazers"/>
@@ -89,8 +90,8 @@ use App\Web\Meta\MetaType;
 <x-template :if="$copyCodeBlocks ?? false">
     <template id="copy-template">
         <button class="absolute group top-2 right-2 size-7 flex items-center justify-center cursor-pointer opacity-0 group-hover:opacity-100 transition text-(--ui-text-dimmed) bg-(--ui-bg-muted) rounded border border-(--ui-border) hover:text-(--ui-text-highlighted)">
-            <x-icon name="tabler:copy" class="size-5 absolute"/>
-            <x-icon name="tabler:copy-check-filled" class="size-5 absolute opacity-0 group-[[data-copied]]:opacity-100 transition"/>
+            <x-icon name="tabler:copy" class="absolute size-5"/>
+            <x-icon name="tabler:copy-check-filled" class="absolute opacity-0 group-[[data-copied]]:opacity-100 size-5 transition"/>
         </button>
     </template>
 </x-template>
