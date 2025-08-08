659cd0486f38c2a30148bb496eebea1898b498a2
brendt <brent.roose@gmail.com>
2025-08-08 11:12:14 +0200

    wip

--- src/Web/Code/code.view.php
+++ src/Web/Code/code.view.php
@@ -18,16 +18,18 @@ use function Tempest\uri;
     </x-slot>
 
     <div class="hero-bg h-full flex justify-center">
-        <x-form :action="uri([CodeController::class, 'submit'])" class="w-full">
-            <x-csrf-token />
-            <div class="grid gap-4 max-full max-w-[66%] mx-auto">
-                <input type="hidden" name="lang" :value="$language" />
-                <label for="code" class="text-xl">Paste your code</label>
-                <textarea name="code" id="code" class="border-2 border-[--primary] w-full p-4 rounded bg-[var(--code-background)] text-[var(--foreground)]" rows="20" autofocus>{{ $code }}</textarea>
-                <div class="flex justify-end">
-                    <input type="submit" class="no-primary rounded-md font-medium inline-flex items-center focus:outline-hidden disabled:cursor-not-allowed aria-disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:opacity-75 transition-colors px-4 py-2 gap-2 ring ring-inset ring-(--ui-border-accented) text-(--ui-text) bg-(--ui-bg) hover:bg-(--ui-bg-elevated) disabled:bg-(--ui-bg) aria-disabled:bg-(--ui-bg) focus-visible:ring-2 focus-visible:ring-(--ui-border-inverted)" />
+        <div class="w-full">
+            <x-form :action="uri([CodeController::class, 'submit'])">
+                <x-csrf-token/>
+                <div class="grid gap-4 max-full max-w-[66%] mx-auto">
+                    <input type="hidden" name="lang" :value="$language"/>
+                    <label for="code" class="text-xl">Paste your code</label>
+                    <textarea name="code" id="code" class="border-2 border-[--primary] w-full p-4 rounded bg-[var(--code-background)] text-[var(--foreground)]" rows="20" autofocus>{{ $code }}</textarea>
+                    <div class="flex justify-end">
+                        <input type="submit" class="no-primary rounded-md font-medium inline-flex items-center focus:outline-hidden disabled:cursor-not-allowed aria-disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:opacity-75 transition-colors px-4 py-2 gap-2 ring ring-inset ring-(--ui-border-accented) text-(--ui-text) bg-(--ui-bg) hover:bg-(--ui-bg-elevated) disabled:bg-(--ui-bg) aria-disabled:bg-(--ui-bg) focus-visible:ring-2 focus-visible:ring-(--ui-border-inverted)"/>
+                    </div>
                 </div>
-            </div>
-        </x-form>
+            </x-form>
+        </div>
     </div>
 </x-base>
