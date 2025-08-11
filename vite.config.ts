2dacc68bdcc25a6eed71ddacc359f64825206710
Enzo Innocenzi <enzo@innocenzi.dev>
2025-08-12 00:22:17 +0200

    fix: watch markdown changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,13 @@ export default defineConfig({
 		tailwindcss(),
 		tempest(),
 		vue(),
+		{
+			name: 'tempest:markdown:watch',
+			handleHotUpdate({ file, server }) {
+				if (file.endsWith('.md')) {
+					server.ws.send({ type: 'full-reload' })
+				}
+			},
+		},
 	],
 })
