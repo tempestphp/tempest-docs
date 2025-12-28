50d3a915b5917fda5844f7391ffcfd7427cbbdc0
Enzo Innocenzi <enzo@innocenzi.dev>
2025-12-28 18:27:06 +0100

    feat: improve header

--- src/Web/x-header.view.php
+++ src/Web/x-header.view.php
@@ -6,68 +6,73 @@ use App\Web\Documentation\DocumentationController;
 
 use function Tempest\Router\is_current_uri;
 
+$isBlog = is_current_uri([BlogController::class, 'show']) || is_current_uri([BlogController::class, 'index']);
 ?>
 
 <!-- Header -->
-<div class="flex flex-col inset-x-0 items-center lg:justify-center z-[1] h-(--ui-header-height)">
-    <header
-            class="group w-full lg:max-w-5xl xl:max-w-7xl 2xl:max-w-8xl fixed lg:rounded-xl lg:-translate-y-2 ring-[transparent] bg-[transparent] ring-1 py-4 px-8 flex items-center justify-between duration-200 lg:data-[scrolling]:translate-y-2 data-[scrolling]:ring-(--ui-border)/90 data-[scrolling]:backdrop-blur data-[scrolling]:bg-(--ui-bg)/75 z-[1]"
-            id="header"
-    >
-        <!-- Left side -->
-        <div class="flex items-center gap-4">
-            <a href="/" class="flex items-center gap-4">
-                <!-- Logo -->
-                <div class="size-8">
-                    <img src="/img/tempest-logo.svg" alt="Tempest logo" class="size-full"/>
-                </div>
-                <span class="font-medium hidden lg:inline">Tempest</span>
-            </a>
+<div class="relative inset-x-0 z-[1] h-(--ui-header-height)">
+  <header
+      class="group transition-[top,border] z-[1] fixed top-4 data-[scrolling]:top-0 flex justify-center bg-[transparent] border-b border-transparent data-[scrolling]:border-(--ui-border) w-full duration-200 data-[scrolling]:backdrop-blur data-[scrolling]:bg-(--ui-bg)/75"
+      id="header"
+  >
+    <div class="flex justify-between items-center px-8 py-4 w-full 2xl:max-w-8xl lg:max-w-5xl xl:max-w-7xl">
+      <!-- Left side -->
+      <div class="flex items-center gap-4">
+        <a href="/" class="flex items-center gap-4">
+          <!-- Logo -->
+          <div class="size-8">
+            <img src="/img/tempest-logo.svg" alt="Tempest logo" class="size-full"/>
+          </div>
+          <span class="hidden lg:inline font-medium">Tempest</span>
+        </a>
 
-            <a class="hidden md:inline text-xs tracking-wide font-medium text-(--ui-text-muted) bg-(--ui-bg)/50 px-2 py-1 rounded-lg border border-(--ui-border)" href="https://github.com/tempestphp/tempest-framework/releases/{{ $latest_release }}">
-                {{ $this->latest_release }}
-            </a>
-        </div>
+        <a class="hidden md:inline text-xs tracking-wide font-medium text-(--ui-text-muted) bg-(--ui-bg)/50 px-2 py-1 rounded-lg border border-(--ui-border)" href="https://github.com/tempestphp/tempest-framework/releases/{{ $latest_release }}">
+          {{ $this->latest_release }}
+        </a>
+      </div>
 
-        <!-- Center -->
-        <div class="flex items-center gap-4">
-            <x-search/>
-        </div>
-        <!-- Right side -->
-        <div class="flex items-center gap-4 font-medium">
-					<a
-						:href="uri([CommunityController::class, 'index'])"
-						class="transition hover:text-(--ui-text-highlighted) <?= is_current_uri([CommunityController::class, 'index'])
-    						? 'md:text-(--ui-primary)'
-    						: '' ?>"
-					>Community</a>
-					<a
-						:href="uri([BlogController::class, 'index'])"
-						class="transition hover:text-(--ui-text-highlighted) <?= is_current_uri([BlogController::class, 'show']) || is_current_uri([BlogController::class, 'index'])
-    						? 'md:text-(--ui-primary)'
-    						: '' ?>"
-					>Blog</a>
-					<a
-						:href="uri([DocumentationController::class, 'index'])"
-						class="transition hover:text-(--ui-text-highlighted) <?= is_current_uri([DocumentationController::class, '__invoke']) ? 'md:text-(--ui-primary)' : '' ?>"
-					>
-						<span class="sm:hidden">Docs</span>
-						<span class="hidden sm:inline">Documentation</span>
-					</a>
-          <a href="https://github.com/tempestphp/tempest-framework" class="transition hover:text-(--ui-text-highlighted) flex items-center gap-x-1.5 ml-4">
-              <x-icon name="tabler:brand-github" class="size-6"/>
-              <span class="font-semibold hidden lg:inline">{{ $this->stargazers_count }}</span>
-          </a>
-        </div>
-    </header>
+      <!-- Center -->
+      <div class="flex items-center gap-4">
+        <x-search />
+      </div>
+      <!-- Right side -->
+      <div class="flex items-center gap-4 font-medium">
+        <a
+          :href="uri([CommunityController::class, 'index'])"
+          class="transition hover:text-(--ui-text-highlighted) <?= is_current_uri([CommunityController::class, 'index']) ? 'md:text-(--ui-primary)' : '' ?>"
+        >
+          Community
+        </a>
+        <a
+          :href="uri([BlogController::class, 'index'])"
+          class="transition hover:text-(--ui-text-highlighted) <?= $isBlog ? 'md:text-(--ui-primary)' : '' ?>"
+        >
+          Blog
+        </a>
+        <a
+          :href="uri([DocumentationController::class, 'index'])"
+          class="transition hover:text-(--ui-text-highlighted) <?= is_current_uri([DocumentationController::class, '__invoke']) ? 'md:text-(--ui-primary)' : '' ?>"
+        >
+          <span class="sm:hidden">Docs</span>
+          <span class="hidden sm:inline">Documentation</span>
+        </a>
+        <a href="https://github.com/tempestphp/tempest-framework" class="transition hover:text-(--ui-text-highlighted) flex items-center gap-x-1.5 ml-4">
+          <x-icon name="tabler:brand-github" class="size-6"/>
+          <span class="hidden lg:inline font-semibold">{{ $this->stargazers_count }}</span>
+        </a>
+      </div>
+    </div>
+  </header>
 </div>
+
 <script>
-    const header = document.getElementById('header')
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 0) {
-            header.dataset.scrolling = true
-        } else {
-            delete header.dataset.scrolling
-        }
-    })
+const header = document.getElementById('header')
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.dataset.scrolling = true
+  } else {
+    delete header.dataset.scrolling
+  }
+})
 </script>
--- src/Web/x-search.view.php
+++ src/Web/x-search.view.php
@@ -1,6 +1,6 @@
-<button toggle-palette class="cursor-pointer hidden sm:block">
+<button toggle-palette class="hidden sm:block cursor-pointer">
     <label for="search" class="sr-only">Search</label>
-    <div class="flex rounded-xl group-[[data-scrolling]]:bg-(--ui-bg) group-[[data-scrolling]]:ring-[transparent] bg-(--ui-bg)/50 dark:bg-[transparent] text-(--ui-text) hover:bg-(--ui-bg-elevated) ring ring-(--ui-border)/80 transition">
+    <div class="flex rounded-xl group-[[data-scrolling]]:bg-(--ui-bg) bg-(--ui-bg)/50 dark:bg-[transparent] text-(--ui-text) hover:bg-(--ui-bg-elevated) ring ring-(--ui-border)/80 transition">
 			<span class="grow px-4 py-2 text-base text-(--ui-text-muted) focus:outline-0 sm:text-sm/6">
 				Search docs, blog...
 			</span>
