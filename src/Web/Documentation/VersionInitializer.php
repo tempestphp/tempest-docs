059ba32493a4bd13d9c2faccdb2e95657b576e2a
brendt <brent.roose@gmail.com>
2026-02-20 08:45:06 +0100

    wip

--- src/Web/Documentation/VersionInitializer.php
+++ src/Web/Documentation/VersionInitializer.php
@@ -5,12 +5,17 @@ namespace App\Web\Documentation;
 use Tempest\Container\Container;
 use Tempest\Container\Initializer;
 use Tempest\Router\MatchedRoute;
+use Throwable;
 
 final class VersionInitializer implements Initializer
 {
     public function initialize(Container $container): Version
     {
-        $version = $container->get(MatchedRoute::class)?->params['version'] ?? null;
+        try {
+            $version = $container->get(MatchedRoute::class)?->params['version'] ?? null;
+        } catch (Throwable) {
+            $version = null;
+        }
 
         if (! $version) {
             return Version::default();
